47d459ac4f340fa99b9d93150ccfc64ec74e2b96
nqhuy <huynguyenquang717@gmail.com>
2026-06-05 21:48:19 +0700

    Sửa giao diện thanh tìm kiếm, logic tìm kiếm

--- src/main/java/com/example/roomrental/controller/ChatController.java
+++ src/main/java/com/example/roomrental/controller/ChatController.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +18,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.web.bind.annotation.PostMapping;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 
 import java.util.List;
 import java.util.Map;
